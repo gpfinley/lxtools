6d03fc78f21807485c2e13fb7a6129640377e0c6
Gregory Finley <gregpfinley@gmail.com>
2019-03-03 14:08:34 -0800

    working changes, more postproc

--- lexicography/configurations.py
+++ lexicography/configurations.py
@@ -1,3 +1,9 @@
+"""
+Simple configuration object and example configurations for Wanano and Barasana.
+Aggressive processing to remove all non-gloss information (examples, PoS, etc.) from entry.
+"""
+
+
 class Config:
 
     def __init__(self,
@@ -12,6 +18,18 @@ class Config:
         self.entry_postproc_replacements = entry_postproc_replacements
         self.encoding = encoding
 
+_capture_example_pattern = '\\s*[eE]j\\. .+?(\\d|$)'
+_capture_example_replace = ' \\1'
+
+_clean_beginning_pattern = '^\\W*'
+_clean_beginning_replace = ''
+
+_clean_end_pattern = '\\s*$'
+_clean_end_replace = ''
+
+_capture_phonetic_and_pos_pattern = '\\[.*?\\](,? *\\S*?\\. )?'
+_capture_phonetic_and_pos_replace = ''
+
 # Saved configurations. Add to this dict to enable simple command-line usage.
 parameters = {
     'wanano': Config(
@@ -19,11 +37,17 @@ parameters = {
         headword_group=2,
         entry_postproc_patterns=(
             '\\n',
-            '\\s*[eE]j\\. .+?(\\d|$)'     # remove example from entry
+            _capture_example_pattern,
+            _capture_phonetic_and_pos_pattern,
+            _clean_beginning_pattern,
+            _clean_end_pattern,
         ),
         entry_postproc_replacements=(
             ' ',
-            ' \\1'
+            _capture_example_replace,
+            _capture_phonetic_and_pos_replace,
+            _clean_beginning_replace,
+            _clean_end_replace,
         ),
         encoding='latin1'
     ),
@@ -33,11 +57,17 @@ parameters = {
         headword_group=2,
         entry_postproc_patterns=(
             '\\n',
-            '\\s*[eE]j\\. .+?(\\d|$)'     # remove example from entry
+            _capture_example_pattern,
+            _capture_phonetic_and_pos_pattern,
+            _clean_beginning_pattern,
+            _clean_end_pattern,
         ),
         entry_postproc_replacements=(
             ' ',
-            ' \\1'
+            _capture_example_replace,
+            _capture_phonetic_and_pos_replace,
+            _clean_beginning_replace,
+            _clean_end_replace,
         ),
         encoding='utf-8'
     )
---
+++ lexicography/do_all.sh
@@ -0,0 +1,2 @@
+python3.6 extraction.py wanano.txt wanano.csv wanano
+python3.6 extraction.py barasana.txt barasana.csv barasana
